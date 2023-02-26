af99953eec737d80b5fc12d87bff5b980079e367
nuth-vireak <timberr.4t@gmail.com>
2023-02-26 23:17:13 +0700

    Initial commit

---
+++ src/main/java/com/example/amigoscodespringbootmasterclass/AmigoscodeSpringBootMasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodespringbootmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeSpringBootMasterClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeSpringBootMasterClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amigoscodespringbootmasterclass/AmigoscodeSpringBootMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodespringbootmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeSpringBootMasterClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
