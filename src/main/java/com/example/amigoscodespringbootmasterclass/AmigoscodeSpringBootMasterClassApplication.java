b21dcc92cbc992a8f8c3a5ca4bc6aa14b52f00c8
nuth-vireak <timberr.4t@gmail.com>
2023-02-27 23:41:50 +0700

    finish lesson 7

--- src/main/java/com/example/amigoscodespringbootmasterclass/AmigoscodeSpringBootMasterClassApplication.java
+++ src/main/java/com/example/amigoscodespringbootmasterclass/AmigoscodeSpringBootMasterClassApplication.java
@@ -2,12 +2,21 @@ package com.example.amigoscodespringbootmasterclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AmigoscodeSpringBootMasterClassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigoscodeSpringBootMasterClassApplication.class, args);
     }
 
+    @GetMapping("/")
+    Customer getCustomer() {
+        return new Customer(1L, "John");
+    }
+
+    record Customer(Long id, String name) {}
 }
