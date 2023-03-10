bc32ea8eb5f5ca385b81acad06f068ca2be2e5f9
nuth-vireak <timberr.4t@gmail.com>
2023-03-10 16:41:46 +0700

    finish lesson 49

--- src/main/java/com/example/amigoscodespringbootmasterclass/customer/CustomerService.java
+++ src/main/java/com/example/amigoscodespringbootmasterclass/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.amigoscodespringbootmasterclass.customer;
 
 import com.example.amigoscodespringbootmasterclass.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import java.util.List;
 @Service
 public class CustomerService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -17,12 +20,20 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long customerId) {
         return customerRepository
                 .findById(customerId)
-                .orElseThrow(() -> new NotFoundException("Customer " + customerId + " was not found"));
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "Customer " + customerId + " was not found");
+                            LOGGER.error("getCustomer was called with id: " + customerId +
+                                    " but customer was not found", notFoundException);
+                            return notFoundException;
+                        });
     }
 }
